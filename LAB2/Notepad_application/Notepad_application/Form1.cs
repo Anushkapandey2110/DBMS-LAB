abc231764527d2fefe3ac5058850413eab91b965
Anushkapandey2110 <124172470+Anushkapandey2110@users.noreply.github.com>
2024-01-13 16:19:48 +0530

    Add files via upload

---
+++ LAB2/Notepad_application/Notepad_application/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Notepad_application
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Title = "Open";
+            dlg.ShowDialog();
+            string fName = dlg.FileName;
+            StreamReader sr = new StreamReader(fName);
+            richTextBox1.Text = sr.ReadToEnd();
+            sr.Close();
+        }
+
+        private void toolStripMenuItem3_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.ShowDialog();
+            string fName = saveFileDialog1.FileName;
+            StreamWriter sw = new StreamWriter(fName);
+            sw.Write(richTextBox1.Text);
+            sw.Flush();
+            sw.Close();
+        }
+
+        private void toolStripMenuItem10_Click(object sender, EventArgs e)
+        {
+            FontDialog fd = new FontDialog();
+            fd.Font = richTextBox1.SelectionFont;
+            fd.Color = richTextBox1.SelectionColor;
+            if (fd.ShowDialog() == DialogResult.OK)
+            {
+                richTextBox1.SelectionFont = fd.Font;
+                richTextBox1.SelectionColor = fd.Color;
+            }
+        }
+
+        private void toolStripMenuItem11_Click(object sender, EventArgs e)
+        {
+            ColorDialog colorDlg = new ColorDialog();  
+    colorDlg.AllowFullOpen = false;  
+    colorDlg.AnyColor = true;  
+    colorDlg.SolidColorOnly = false;  
+    colorDlg.Color = Color.Red;
+
+    if (colorDlg.ShowDialog() == DialogResult.OK)
+    {
+        richTextBox1.ForeColor = colorDlg.Color;
+    }
+        }
+    }
+}
